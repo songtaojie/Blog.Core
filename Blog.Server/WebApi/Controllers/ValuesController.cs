2c30af97c995fc99bdf26956a9dd1cc6f6eeb072
stjworkemail <stjworkemail@163.com>
2019-08-11 23:05:14 +0800

    初始化项目结构

--- Blog.Server/WebApi/Controllers/ValuesController.cs
+++ Blog.Server/WebApi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Blog.Core.Controllers
+namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
---
+++ Blog.Server/WebApi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                var host = CreateWebHostBuilder(args).Build();
+                logger.Info("应用程序启动成功!");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+
+                logger.Error(ex, "Stopped program");
+                throw;
+            }
+            finally
+            {
+                // 确保在应用程序退出之前刷新和停止内部计时器/线程（避免Linux上的分段错误）
+                NLog.LogManager.Shutdown();
+            }
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();
+    }
+}
