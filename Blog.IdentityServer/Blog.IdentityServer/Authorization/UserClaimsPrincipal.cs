33512bd9962c6e09d3fe9c9a46c6c33a7bf2a876
songtaojie <songtaojie@hnyotech.com.cn>
2020-07-13 16:09:23 +0800

    添加用户DContex

---
+++ Blog.IdentityServer/Blog.IdentityServer/Authorization/UserClaimsPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blog.IdentityServer.Authorization
+{
+    public class UserClaimsPrincipal : IUserClaimsPrincipalFactory<IdentityUser>
+    {
+        //private readonly IUserStoreService _storeService;
+        //public UserClaimsPrincipal(IUserStoreService storeService)
+        //{
+        //    _storeService = storeService;
+        //}
+        public async Task<ClaimsPrincipal> CreateAsync(IdentityUser user)
+        {
+            //var claims = await _storeService.GetAllClaimsByUser(user);
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim>());
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            return await Task.FromResult(claimsPrincipal);
+
+        }
+    }
+}
--- Blog.IdentityServer/Blog.IdentityServer/Startup.cs
+++ Blog.IdentityServer/Blog.IdentityServer/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.AspNetCore.Identity;
 using Blog.IdentityServer.Data;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
+using Blog.IdentityServer.Authorization;
 
 namespace Blog.IdentityServer
 {
@@ -38,72 +39,36 @@ namespace Blog.IdentityServer
             var connectionString = Configuration.GetConnectionString("SqlServerConnection");
             if (string.IsNullOrEmpty(connectionString)) throw new Exception("数据库配置异常");
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            //// 数据库配置系统应用用户数据上下文
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            // 启用 Identity 服务 添加指定的用户和角色类型的默认标识系统配置
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
             var builder = services.AddIdentityServer()
-            //.AddAspNetIdentity<ApplicationUser>()
-            .AddTestUsers(IdentityServerHost.Quickstart.UI.TestUsers.Users)
+            .AddAspNetIdentity<ApplicationUser>()
+            //.AddTestUsers(IdentityServerHost.Quickstart.UI.TestUsers.Users)
             //// 添加配置数据（客户端 和 资源）
             .AddConfigurationStore(options =>
             {
-                options.ConfigureDbContext = b =>
-                    b.UseSqlServer(connectionString,
-                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.ConfigureDbContext = b =>b.UseSqlServer(connectionString,sql => sql.MigrationsAssembly(migrationsAssembly));
             })
             .AddOperationalStore(options =>
             {
-                options.ConfigureDbContext = b =>
-                    b.UseSqlServer(connectionString,
-                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.ConfigureDbContext = b =>b.UseSqlServer(connectionString,sql => sql.MigrationsAssembly(migrationsAssembly));
                 // 自动清理 token ，可选
                 options.EnableTokenCleanup = true;
             });
 
-            ////// 数据库配置系统应用用户数据上下文
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
-            //// 启用 Identity 服务 添加指定的用户和角色类型的默认标识系统配置
-            //services.AddIdentity<ApplicationUser, IdentityRole>()
-            //    .AddEntityFrameworkStores<ApplicationDbContext>()
-            //    .AddDefaultTokenProviders();
-
+           
             builder.AddDeveloperSigningCredential();
             if (Environment.IsDevelopment())
             {
                 builder.AddDeveloperSigningCredential();
             }
             services.AddAuthorization();
-            ////.AddTestUsers(InMemoryConfig.Users().ToList())
-            ////.AddInMemoryApiResources(InMemoryConfig.GetApiResources())
-            ////.AddInMemoryClients(InMemoryConfig.GetClients());
-            //builder.AddDeveloperSigningCredential();
-            //services.AddAuthentication();
-
-            //services.AddMvc();
-
-            //var builder = services.AddIdentityServer(options =>
-            //{
-            //    options.Events.RaiseErrorEvents = true;
-            //    options.Events.RaiseInformationEvents = true;
-            //    options.Events.RaiseFailureEvents = true;
-            //    options.Events.RaiseSuccessEvents = true;
-            //})
-            //// in-memory, code config
-            //.AddTestUsers(InMemoryConfig.Users().ToList())
-            //.AddInMemoryApiResources(InMemoryConfig.GetApiResources())
-            //.AddInMemoryClients(InMemoryConfig.GetClients());
-
-
-            //builder.AddDeveloperSigningCredential();
-
-            //if (Environment.IsDevelopment())
-            //{
-            //    builder.AddDeveloperSigningCredential();
-            //}
-            //else
-            //{
-            //    throw new Exception("need to configure key material");
-            //}
-
-            //services.AddAuthentication();//配置认证服务
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
