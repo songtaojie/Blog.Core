e89f0c0f13737f63a11974c88d00a8c9cd7a2b93
songtaojie <songtaojie@hnyotech.com.cn>
2020-07-17 17:17:08 +0800

    添加用户

---
+++ Blog.IdentityServer/Blog.IdentityServer/Models/Bak/RootEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.IdentityServer.Models
+{
+    public class RootEntity
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+
+    }
+}
